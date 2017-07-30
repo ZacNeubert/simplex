39d89f75e3df89dba47b9d4eb08d18a47f309274
Zachary Neubert <zacneubert@gmail.com>
2017-07-30 10:47:46 -0600

    Working Simplex (brute force).

--- parse_matrix.py
+++ parse_matrix.py
@@ -3,11 +3,26 @@
 from sys import argv
 import numpy as np
 
-def matrix_from_file(f):
-    with open(f, 'r') as inf:
-        lines = [[int(i) for i in line if i.isnumeric()] for line in inf]
-        print(lines)
+def is_int(value):
+    try:
+        int(value)
+        return True
+    except ValueError:
+        return False
+
+def int_line(line):
+    return [int(i) for i in line.split(' ') if is_int(i)]
+
+def matrix_from_lines(lines):
+    lines = [int_line(line) for line in lines if '#' not in line]
     return np.matrix(lines)
 
-if __name__ == '__main__':
-    print(matrix_from_file('mat.txt'))
+def parse_problem(f):
+    with open(f, 'r') as inf:
+        lines = inf.readlines()
+        A_lines = lines[1:]
+        A = matrix_from_lines(A_lines)
+        z_coeffs = int_line(lines[0])
+        b = A[:, -1]
+        A = A[:, :-1]
+        return A, b, z_coeffs
--- simplex.py
+++ simplex.py
@@ -1,14 +1,29 @@
 #!/usr/bin/python3
 
-from parse_matrix import matrix_from_file
+from parse_matrix import parse_problem
 from itertools import combinations
+from functools import reduce
 import numpy as np
+
+from sys import argv
+
+
+def has_inverse(matrix):
+    try:
+        matrix.I
+        return True
+    except np.linalg.LinAlgError:
+        return False
+
 
 class FeasibleBase:
     def __init__(self, A, b, z_coeffs, columns):
         self.A = A
         self.b = b
         self.z_size = len(z_coeffs)
+        self.z_coeffs = z_coeffs
+        self.b_columns = columns
+
         self.b_values = [i for i in range(self.z_size) if i in columns]
         self.n_values = [i for i in range(self.z_size) if i not in columns]
 
@@ -17,19 +32,67 @@ class FeasibleBase:
 
         self.B_i = self.B.I
 
-        self.c_b = [z_coeffs[i] for i in self.b_values]
-        self.c_n = [z_coeffs[i] for i in self.n_values]
+        self.c_b = np.matrix([z_coeffs[i] for i in self.b_values])
+        self.c_n = np.matrix([z_coeffs[i] for i in self.n_values])
 
-        one = self.c_b * self.B_i 
-        two = one * self.b.T
-        three = two - (self.c_b * self.B_i * self.N)*self.c_n
+    def z(self):
+        z_positive = self.c_b * self.B_i * self.b
+        z_negative = -1 * (self.c_b * self.B_i * self.N - self.c_n) * self.x_N()
+        z = (z_positive + z_negative)[0, 0]
+        return z
 
     def a_(self, n):
-        return self.N[: n]
+        return self.A[:, n]
+
+    def x_B(self):
+        return self.B_i * self.b
+
+    def x_N(self):
+        return np.matrix([[0 for i in range(self.N.shape[1])]]).T
+
+    def cbbi(self):
+        return self.c_b * self.B_i
+
+    def cbbiaj(self, j):
+        aj = self.a_(j)
+        cbbi = self.cbbi()
+        return cbbi * aj
+
+    def cbbiajcj(self, j):
+        return self.cbbiaj(j) - self.z_coeffs[j]
 
     def is_optimal(self):
-        o = [self.a_(n) for n in self.n_values]
-        return reduce(lambda is_optimal, fits: is_optimal and fits, o)
+        is_optimal = True
+        for j in range(0, self.z_size):
+            val = -1 * (self.cbbiajcj(j))
+            if val > 0:
+                is_optimal = False
+                break
+        return is_optimal
+
+    def is_non_negative(self):  # Do not violate non-negative constraints
+        return reduce(lambda total, new: total and new, [b >= 0 for b in self.x_B()])
+
+    def answer_string(self):
+        zstr = 'z = {}\n'.format(self.z())
+
+        basis_i = 0
+        other_vars = []
+        for i in range(len(self.z_coeffs)):
+            if i in self.b_columns:
+                other_vars.append(self.x_B()[basis_i, 0])
+                basis_i += 1
+            else:
+                other_vars.append(0)
+
+        other_vars = ['x{} = {}'.format(i + 1, v) for i, v in enumerate(other_vars)]
+        return zstr + '\n'.join(other_vars)
+
+    def __str__(self):
+        return 'z: {}\tx_B: {}'.format(self.z(), self.x_B())
+
+    def __repr__(self):
+        return str(self)
 
 
 class MatrixSimplex:
@@ -43,24 +106,20 @@ class MatrixSimplex:
         A_shape = self.A.shape
         B_size = A_shape[0]
         total_cols = A_shape[1]
-        
-        feasible_bases = []
-        for columns in combinations(range(total_cols), B_size):
-            try:
-                self.A[:, columns].I # Make sure I exists
-                feasible_bases.append(FeasibleBase(self.A, self.b, self.z_coeffs, columns))
-            except np.linalg.LinAlgError:
-                pass # I did not exist
 
-        return feasible_B
+        feasible_bases = [FeasibleBase(self.A, self.b, self.z_coeffs, columns) for columns in
+                          combinations(range(total_cols), B_size) if has_inverse(self.A[:, columns])]
+        feasible_bases = [f for f in feasible_bases if f.is_non_negative()]
+
+        feasible_B = [f for f in feasible_bases if f.is_optimal()]
+
+        assert len(feasible_B) == 1
+        return feasible_B[0]
+
 
 if __name__ == '__main__':
-    augmented_mat = matrix_from_file('p2.txt')
-    b = augmented_mat[:, -1]
-    mat = augmented_mat[:, :-1]
-    print(mat, b)
+    filename = argv[1]
+    A, b, z_coeffs = parse_problem(filename)
+    simplex = MatrixSimplex(z_coeffs, A, b)
 
-    z_coeffs = [3, 1, 0, 0, 0, 0]
-    simplex = MatrixSimplex(z_coeffs, mat, b)
-
-    print(simplex.doSimplex())
+    print(simplex.doSimplex().answer_string())
